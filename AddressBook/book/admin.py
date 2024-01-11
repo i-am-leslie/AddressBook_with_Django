34a00b5083adbc7656378b2f4f8045542de0b312
ejehleslie <ejehboss@gmail.com>
2024-01-11 06:23:30 +0000

    first commit

---
+++ AddressBook/book/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import AddressBook,BuddyInfo
+
+admin.site.register(AddressBook)
+admin.site.register(BuddyInfo)
---
+++ AddressBook/book/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+class AddressBook(models.Model):
+    name=models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name_plural = 'AddressBooks'
+    
+    def __str__(self):
+        return self.name
+
+class BuddyInfo(models.Model):
+    name=models.CharField(max_length=255)
+    Address=models.TextField()
+    Phone_Number=models.IntegerField()
+    Addres_book= models.ForeignKey(AddressBook,related_name='BuddyInfo',on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
