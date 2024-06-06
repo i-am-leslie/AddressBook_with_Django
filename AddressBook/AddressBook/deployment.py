1c6b5f181739681d9c56c35109d005703681af38
ejehleslie <ejehboss@gmail.com>
2024-06-06 14:53:03 -0400

    Made few changes

--- AddressBook/AddressBook/deployment.py
+++ AddressBook/AddressBook/deployment.py
@@ -2,8 +2,8 @@ import os
 from .settings import *
 from .settings import BASE_DIR
 
-ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
-CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']]
+ALLOWED_HOSTS = [os.environ['addressbookdjango.azurewebsites.net']]
+CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['addressbookdjango.azurewebsites.net']]
 DEBUG = False
 
 
