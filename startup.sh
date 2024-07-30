672d04d856b5af930f3eb9b0f41b22aa3fdedab0
ejehleslie <ejehboss@gmail.com>
2024-07-30 12:52:52 -0400

    startup

--- startup.sh
+++ startup.sh
@@ -3,6 +3,9 @@
 # Install gettext
 apt-get update && apt-get install -y gettext
 
+# Compile the text 
+django-admin compilemessages
+
 # Start the web app using gunicorn
 # Replace 'AddressBook.wsgi' with your project's WSGI module path
 gunicorn --bind=0.0.0.0 --timeout 600 --chdir AddressBook AddressBook.wsgi
