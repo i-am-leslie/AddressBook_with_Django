f25136a506d570d6e7b47c1324a98002068c1ecf
ejehleslie <ejehboss@gmail.com>
2024-08-01 00:12:00 -0400

    Fix

---
+++ AddressBook/startup.sh
@@ -0,0 +1,9 @@
+# !/bin/bash
+
+# Install gettext
+apt-get update && apt-get install -y gettext
+
+
+# Start the web app using gunicorn
+# Replace 'AddressBook.wsgi' with your project's WSGI module path
+gunicorn --bind=0.0.0.0 --timeout 600 --chdir AddressBook AddressBook.wsgi
